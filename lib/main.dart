22d8a3c1a7fd7bf02820c84bb9c9d3269a60b084
ShahnawazDev <93546124+ShahnawazDev@users.noreply.github.com>
2023-09-17 22:37:27 +0530

    Added app icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-import 'package:nyay/pages/on_boarding_page.dart';
+import 'package:nyay/pages/splash_screen_page.dart';
 
-void main() => runApp(const App());
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const App());
+}
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -27,7 +32,7 @@ class App extends StatelessWidget {
           home: child,
         );
       },
-      child: const OnBoardingPage(),
+      child: const SplashScreen(),
     );
   }
 }
